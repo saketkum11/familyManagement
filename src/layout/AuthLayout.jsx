bfa432a50d652ce1fb2acc37b0c383d2105d031a
Saket kumar <kumarsaket601@gmail.com>
2024-03-12 18:28:23 +0530

    initial commit

---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLayout = () => {
+  return <section></section>;
+};
+
+export default AuthLayout;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>loging</div>;
+};
+
+export default Login;
